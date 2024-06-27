0dc91bc503a9d3fdc4e1a0f6f3536f97609002bc
sagar singh <sagar.17jccs049@jietjodhpur.ac.in>
2024-06-27 19:20:15 +0530

    add localstorage hook for todo.

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,18 @@
+import { useEffect, useReducer } from "react";
+import { ActionType, DefaultTypes } from "../context/TodoContext";
+
+export function useLocalStorage(key:string,reducer:React.Reducer<DefaultTypes,ActionType>,initialState:DefaultTypes){
+    const [state,dispatch] = useReducer(reducer,initialState,(initial)=>{
+        console.log("localStorage:",initial);
+        const jsonData = localStorage.getItem(key)
+        if(jsonData !== null) return JSON.parse(jsonData)
+        else return initial
+    })
+
+    useEffect(()=>{
+        localStorage.setItem(key,JSON.stringify(state))
+    },[state])
+
+    return [state,dispatch] as const
+
+}
