aac275b81962eff28dde0da343c93b532b623778
sagar singh <sagar.17jccs049@jietjodhpur.ac.in>
2024-06-27 19:26:00 +0530

    remove console.logs

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -3,7 +3,6 @@ import { ActionType, DefaultTypes } from "../context/TodoContext";
 
 export function useLocalStorage(key:string,reducer:React.Reducer<DefaultTypes,ActionType>,initialState:DefaultTypes){
     const [state,dispatch] = useReducer(reducer,initialState,(initial)=>{
-        console.log("localStorage:",initial);
         const jsonData = localStorage.getItem(key)
         if(jsonData !== null) return JSON.parse(jsonData)
         else return initial
